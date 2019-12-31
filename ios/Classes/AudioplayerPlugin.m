c7dd7a0422cf770fb692f954ff4a886bf123b16c
weifucheng <39686870+weifucheng1102@users.noreply.github.com>
2019-12-31 17:00:58 +0800

    Update AudioplayerPlugin.m
    锁屏 显示播放数据

--- ios/Classes/AudioplayerPlugin.m
+++ ios/Classes/AudioplayerPlugin.m
@@ -7,6 +7,7 @@ static NSString *const CHANNEL_NAME = @"bz.rxla.flutter/audio";
 static FlutterMethodChannel *channel;
 static AVPlayer *player;
 static AVPlayerItem *playerItem;
+static NSMutableArray * arr;
 
 @interface AudioplayerPlugin()
 -(void)pause;
@@ -26,8 +27,10 @@ BOOL isPlaying = false;
 NSMutableSet *observers;
 NSMutableSet *timeobservers;
 FlutterMethodChannel *_channel;
-
-
+BOOL isback = false;
+int playIndex;
+int  playedTime = 0;
+int playState = 0;
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     FlutterMethodChannel* channel = [FlutterMethodChannel
@@ -38,7 +41,6 @@ FlutterMethodChannel *_channel;
     _channel = channel;
     [registrar addApplicationDelegate:instance];
 }
-
 -(void)applicationWillResignActive:(UIApplication *)application
 {
     [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
@@ -47,13 +49,68 @@ FlutterMethodChannel *_channel;
     [session setActive:YES error:nil];
     [session setCategory:AVAudioSessionCategoryPlayback error:nil];
 }
+//设置锁屏信息
+- (void)setLockingInfo
+{
+    if (player) {
+        if (!isback || arr.count==0) {
+            [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:nil];
+            return;
 
+        }
+         //数据信息
+           NSMutableDictionary *songInfo = [[NSMutableDictionary alloc] init];
+           
+           //图片
+
+           NSData *imgData = [NSData dataWithContentsOfURL:[NSURL URLWithString:arr[playIndex][@"musicimg"]]];
+           UIImage *image = [UIImage imageWithData:imgData];
+           if (image) {
+               MPMediaItemArtwork *albumArt = [[MPMediaItemArtwork alloc] initWithImage:image];
+               [songInfo setObject:albumArt forKey:MPMediaItemPropertyArtwork];
+           }
+
+           
+           
+           // 4、设置歌曲的时长和已经消耗的时间
+              NSNumber *playbackDuration = @(CMTimeGetSeconds(player.currentItem.duration));
+              NSNumber *elapsedPlaybackTime = @(CMTimeGetSeconds(player.currentItem.currentTime));
+
+              if (!playbackDuration || !elapsedPlaybackTime) {
+                  return;
+              }
+           //当前播放时间
+           [songInfo setObject:elapsedPlaybackTime forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime];
+           //速率
+           [songInfo setObject:[NSNumber numberWithFloat:1.0f] forKey:MPNowPlayingInfoPropertyPlaybackRate];
+           //剩余时长
+           [songInfo setObject:playbackDuration forKey:MPMediaItemPropertyPlaybackDuration];
+           
+           //设置标题
+           [songInfo setObject:@"亲道" forKey:MPMediaItemPropertyTitle];
+           
+           //设置副标题
+           [songInfo setObject:arr[playIndex][@"title"] forKey:MPMediaItemPropertyArtist];
+           
+           //设置音频数据信息
+           [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:songInfo];
+        
+    }else{
+         [[MPNowPlayingInfoCenter defaultCenter] setNowPlayingInfo:nil];
+    }
+   
+}
 -(void)applicationDidEnterBackground:(UIApplication *)application
 {
+    isback  =true;
     [self setupLockScreenControlInfo];
 }
+-(void)applicationDidBecomeActive:(UIApplication *)application{
+    isback = false;
+}
+
 - (void)setupLockScreenControlInfo {
-    
+    [self setLockingInfo];
     MPRemoteCommandCenter *commandCenter = [MPRemoteCommandCenter sharedCommandCenter];
     
     // 锁屏播放
@@ -104,66 +161,76 @@ FlutterMethodChannel *_channel;
     }];
     
     
-    /*    这部分功能没有用到，就暂时先放在这里
-     // 上一曲
-     MPRemoteCommand *previousCommand = commandCenter.previousTrackCommand;
-     [previousCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+//        //这部分功能没有用到，就暂时先放在这里
+//     // 上一曲
+//     MPRemoteCommand *previousCommand = commandCenter.previousTrackCommand;
+//    if (playIndex == 0) {
+//        previousCommand.enabled = NO;
+//    }else{
+//        previousCommand.enabled = YES;
+//    }
+//     [previousCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+//         if (isPlaying) {
+//            [self stop];
+//         }
+//         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//         [self playBack];
+//         });
+//     return MPRemoteCommandHandlerStatusSuccess;
+//     }];
+//
+//     // 下一曲
+//     MPRemoteCommand *nextCommand = commandCenter.nextTrackCommand;
+//    if (playIndex==arr.count-1) {
+//        nextCommand.enabled = NO;
+//    }else{
+//        nextCommand.enabled = YES;
+//
+//    }
+//     [nextCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+//     if (isPlaying) {
+//        [self stop];
+//     }
+//
+//     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//
+//
+//     [self playNext];
+//     });
+//
+//     return MPRemoteCommandHandlerStatusSuccess;
+//     }];
+//
+//     // 快进
+//     MPRemoteCommand *forwardCommand = commandCenter.seekForwardCommand;
+//     forwardCommand.enabled = YES;
+//     [forwardCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+//
+//     MPSeekCommandEvent *seekEvent = (MPSeekCommandEvent *)event;
+//     if (seekEvent.type == MPSeekCommandEventTypeBeginSeeking) {
+//     [self seekingForwardStart];
+//     }else {
+//     [self seekingForwardStop];
+//     }
+//
+//     return MPRemoteCommandHandlerStatusSuccess;
+//     }];
      
-     [self playPrevMusic];
-     
-     return MPRemoteCommandHandlerStatusSuccess;
-     }];
-     
-     // 下一曲
-     MPRemoteCommand *nextCommand = commandCenter.nextTrackCommand;
-     nextCommand.enabled = YES;
-     [nextCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
-     
-     self.isAutoPlay = NO;
-     
-     if (self.isPlaying) {
-     [self stopMusic];
-     }
-     
-     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-     
-     
-     [self playNextMusic];
-     });
-     
-     return MPRemoteCommandHandlerStatusSuccess;
-     }];
-     
-     // 快进
-     MPRemoteCommand *forwardCommand = commandCenter.seekForwardCommand;
-     forwardCommand.enabled = YES;
-     [forwardCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
-     
-     MPSeekCommandEvent *seekEvent = (MPSeekCommandEvent *)event;
-     if (seekEvent.type == MPSeekCommandEventTypeBeginSeeking) {
-     [self seekingForwardStart];
-     }else {
-     [self seekingForwardStop];
-     }
-     
-     return MPRemoteCommandHandlerStatusSuccess;
-     }];
-     
-     // 快退
-     MPRemoteCommand *backwardCommand = commandCenter.seekBackwardCommand;
-     backwardCommand.enabled = YES;
-     [backwardCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
-     
-     MPSeekCommandEvent *seekEvent = (MPSeekCommandEvent *)event;
-     if (seekEvent.type == MPSeekCommandEventTypeBeginSeeking) {
-     [self seekingBackwardStart];
-     }else {
-     [self seekingBackwardStop];
-     }
-     
-     return MPRemoteCommandHandlerStatusSuccess;
-     }];
-     */
+//     // 快退
+//     MPRemoteCommand *backwardCommand = commandCenter.seekBackwardCommand;
+//     backwardCommand.enabled = YES;
+//     [backwardCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
+//
+//     MPSeekCommandEvent *seekEvent = (MPSeekCommandEvent *)event;
+//     if (seekEvent.type == MPSeekCommandEventTypeBeginSeeking) {
+//     [self seekingBackwardStart];
+//     }else {
+//     [self seekingBackwardStop];
+//     }
+//
+//     return MPRemoteCommandHandlerStatusSuccess;
+//     }];
+//
     
      //拖动进度条
 //    if (@available(iOS 9.1, *)) {
@@ -196,16 +263,31 @@ FlutterMethodChannel *_channel;
 //        // Fallback on earlier versions
 //    }
 }
-
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
     typedef void (^CaseBlock)(void);
     // Squint and this looks like a proper switch!
     NSDictionary *methods = @{
                               @"play":
                                   ^{
+                                      
                                       NSString *url = call.arguments[@"url"];
                                       int isLocal = [call.arguments[@"isLocal"] intValue];
+                                      NSString *name = call.arguments[@"name"];
+                                      arr = [NSMutableArray arrayWithCapacity:0];
+                                      NSString * jsonStr = call.arguments[@"json"];
+                                      //string转data
+                                    NSData * jsonData = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
+                                          //json解析
+                                    id obj = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:nil];
+                                    arr = obj;
+                                      playIndex = 0;
+                                      for (int i = 0; i<arr.count; i++) {
+                                          if([name isEqualToString:arr[i][@"title"]]){
+                                              playIndex = i;
+                                          }
+                                      }
                                       [self play:url isLocal:isLocal];
+
                                       result(nil);
                                   },
                               @"pause":
@@ -227,6 +309,11 @@ FlutterMethodChannel *_channel;
                                   ^{
                                       [self seek:CMTimeMakeWithSeconds([call.arguments doubleValue], 1)];
                                       result(nil);
+                                  },
+                              @"changeState":
+                                  ^{
+                                      [self changeState:[call.arguments intValue]];
+                                      result(nil);
                                   }
                               };
     
@@ -238,7 +325,12 @@ FlutterMethodChannel *_channel;
     }
 }
 
+//修改播放状态 -1不循环   0单曲循环   1列表循环
+-(void)changeState:(int)state{
+    playState = state;
+}
 - (void)play:(NSString*)url isLocal:(int)isLocal {
+    playedTime = 0;
     if (![url isEqualToString:lastUrl]) {
         [playerItem removeObserver:self
                         forKeyPath:@"player.currentItem.status"];
@@ -287,6 +379,7 @@ FlutterMethodChannel *_channel;
     [self onStart];
     [player play];
     isPlaying = true;
+
 }
 
 - (void)onStart {
@@ -300,6 +393,12 @@ FlutterMethodChannel *_channel;
 - (void)onTimeInterval:(CMTime)time {
     int mseconds =  CMTimeGetSeconds(time)*1000;
     [_channel invokeMethod:@"audio.onCurrentPosition" arguments:@(mseconds)];
+//    if (playedTime<10) {
+    if (isPlaying) {
+        [self setLockingInfo];
+    }
+//        playedTime = playedTime+1;
+//    }
 }
 
 - (void)pause {
@@ -343,6 +442,19 @@ FlutterMethodChannel *_channel;
                               context:context];
     }
 }
+
+
+-(void)playNext{
+    playIndex = playIndex+1;
+    playedTime = 0;
+    [self play:arr[playIndex][@"vipmusic"] isLocal:NO];
+}
+-(void)playBack{
+    playIndex = playIndex-1;
+    playedTime = 0;
+    [self play:arr[playIndex][@"vipmusic"] isLocal:NO];
+}
+
 
 - (void)dealloc {
     for (id ob in timeobservers) {
